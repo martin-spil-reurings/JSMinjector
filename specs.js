5ea9d14e44b348121fbd8d4cfd990f96f528c313
Martin Reurings <martin@windgazer.nl>
2012-07-30 15:55:23 +0200

    Example specs.js, which should be used to add specs to the inspector setup.

---
+++ specs.js
@@ -0,0 +1,11 @@
+/**
+ * This is an example setup for adding specs to the injection sequence.
+ * At the verry least it should call the JSMinjector.addSpecs method with an array
+ * of specs. Other code may be added, for instance to load some custom CSS.
+ * See the injector.js file for more information (such as, what settings are
+ * availble from JSMinjector...
+ * 
+ * @fileoverview 
+ */
+
+JSMinjector.addSpecs( ["/Testing.spec.js"] );
